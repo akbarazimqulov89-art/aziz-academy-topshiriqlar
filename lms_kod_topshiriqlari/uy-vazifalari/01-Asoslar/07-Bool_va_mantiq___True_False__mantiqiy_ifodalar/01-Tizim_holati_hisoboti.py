3b06da51650086f0805f86164e0a0e075d54ed5e
akbarazimqulov89-art <akbarazimqulov89@gmail.com>
2026-07-03 14:00:37 +0300

    Add: Tizim holati hisoboti [akbarazimqulov89-art]

--- lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Tizim_holati_hisoboti.py
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Tizim_holati_hisoboti.py
@@ -1,4 +1,9 @@
 server_ishlayapti = True
 baza_ulangan = True
 xatolik_bor = False
-xavsiz = True
+xavfsiz = True
+print("=== TIZIM HOLATI ===")
+print("Server ishlayapti:", server_ishlayapti)
+print("Baza ulangan:", baza_ulangan)
+print("Xatolik bor:", xatolik_bor)
+print("Xavfsiz:", xavfsiz)
