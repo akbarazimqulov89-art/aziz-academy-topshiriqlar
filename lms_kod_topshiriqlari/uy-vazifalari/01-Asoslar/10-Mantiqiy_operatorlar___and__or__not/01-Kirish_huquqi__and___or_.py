1a05a6f662385683a59e924c9865271b06a7e323
akbarazimqulov89-art <akbarazimqulov89@gmail.com>
2026-07-03 16:26:20 +0300

    Add: Kirish huquqi (and / or) [akbarazimqulov89-art]

--- lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/10-Mantiqiy_operatorlar___and__or__not/01-Kirish_huquqi__and___or_.py
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/10-Mantiqiy_operatorlar___and__or__not/01-Kirish_huquqi__and___or_.py
@@ -1,2 +1,5 @@
-is_mumber = True
-has_ticet = False
+is_member = True
+has_ticket = False
+
+print(f"Ikkalasi ham (and): {is_member and has_ticket}")
+print(f"Kamida bittasi (or): {is_member or has_ticket}")
