42dac12cd6560ae1f2d7733cbe41fdb841d90b42
akbarazimqulov89-art <akbarazimqulov89@gmail.com>
2026-07-03 15:12:42 +0300

    Add: Profil kartasi [akbarazimqulov89-art]

--- lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/01-Profil_kartasi.py
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/01-Profil_kartasi.py
@@ -3,9 +3,10 @@ familiya = "Karimov"
 kurs = "Python"
 daraja = "Boshlang'ich"
 shahar = "Toshkent"
+
 print("=== PROFIL ===")
 print("Ism:", ism)
 print("Familiya:", familiya)
-print("Kurs", kurs)
+print("Kurs:", kurs)
 print("Daraja:", daraja)
 print("Shahar:", shahar)
