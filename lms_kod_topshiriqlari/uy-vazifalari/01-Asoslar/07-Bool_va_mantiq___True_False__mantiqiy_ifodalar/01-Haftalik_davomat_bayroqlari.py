2ea1cd974f0d8834726d1300472036e7666ac6a0
akbarazimqulov89-art <akbarazimqulov89@gmail.com>
2026-07-03 15:07:23 +0300

    Add: Haftalik davomat bayroqlari [akbarazimqulov89-art]

--- lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Haftalik_davomat_bayroqlari.py
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Haftalik_davomat_bayroqlari.py
@@ -3,3 +3,9 @@ seshanba = True
 chorshanba = False
 payshanba = True
 juma = False
+
+print(f"Dushanba: {dushanba}")
+print(f"Seshanba: {seshanba}")
+print(f"Chorshanba: {chorshanba}")
+print(f"Payshanba: {payshanba}")
+print(f"Juma: {juma}")
