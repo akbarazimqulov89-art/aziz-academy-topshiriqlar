80962d0d2b760b9ae2b1cde839d1ae5e3989d6c4
akbarazimqulov89-art <akbarazimqulov89@gmail.com>
2026-07-04 09:05:47 +0300

    Add: Do'kon cheki (int va float birga) [akbarazimqulov89-art]

--- lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/05-Sonlar__int_va_float___butun_va_kasr_sonlar/01-Do_kon_cheki__int_va_float_birga_.py
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/05-Sonlar__int_va_float___butun_va_kasr_sonlar/01-Do_kon_cheki__int_va_float_birga_.py
@@ -1,4 +1,10 @@
-mahsulot1 = "Non"
+mahsulot = "Non"
 narx1 = 4500
+
 mahsulot2 = "Sut"
 narx2 = 12.5
+
+print("=== CHEK ===")
+print(mahsulot, "-", narx1)
+print(mahsulot2, "-", narx2)
+print("Rahmat!")
