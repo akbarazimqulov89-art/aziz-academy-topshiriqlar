d1e66f466f240364b64136bcdc10de6c990e9578
akbarazimqulov89-art <akbarazimqulov89@gmail.com>
2026-07-25 09:22:13 +0300

    Add AI task: O'rtacha hosili

---
+++ ai_topshiriqlar/Ortacha_hosili.py
@@ -0,0 +1,18 @@
+# O'rtacha hosili
+# Kurs: Dasturlash / IT
+# Mavzu: Dasturlashga kirish — Python nima va nega o'rganamiz
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+
+sonlar = [a, b, c, d, e]
+
+sonlar.remove(max(sonlar))
+sonlar.remove(min(sonlar))
+
+print(sum(sonlar) // 3)
