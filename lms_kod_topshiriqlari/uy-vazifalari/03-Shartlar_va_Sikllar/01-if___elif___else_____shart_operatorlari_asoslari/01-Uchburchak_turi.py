0527a67332c39973320c5231351978efe550964a
akbarazimqulov89-art <akbarazimqulov89@gmail.com>
2026-08-15 19:59:58 +0300

    Add: Uchburchak turi [akbarazimqulov89-art]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/01-if___elif___else_____shart_operatorlari_asoslari/01-Uchburchak_turi.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+# Uchburchak mavjudlik sharti (tomonlar musbat va har ikkisining yig'indisi uchinchisidan katta)
+if a > 0 and b > 0 and c > 0 and (a + b > c) and (a + c > b) and (b + c > a):
+    if a == b == c:
+        print("teng tomonli")
+    elif a == b or b == c or a == c:
+        print("teng yonli")
+    else:
+        print("turli tomonli")
+else:
+    print("notogri")
