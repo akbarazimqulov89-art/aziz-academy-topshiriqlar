1e5670469de64ae8b38d147f8012614de87e1216
akbarazimqulov89-art <akbarazimqulov89@gmail.com>
2026-08-16 20:42:30 +0300

    Add: 4) Ikki son tengligi [akbarazimqulov89-art]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/02-Ichma-ich_shartlar__nested_if____mantiqni_tartibla/04-4__Ikki_son_tengligi.py
@@ -0,0 +1,10 @@
+parts = input().split()
+a = int(parts[0])
+b = int(parts[1])
+if a == b:
+    if a ==0:
+        print("Equal Zero")
+    else:
+        print("Equal")
+else:
+    print("Not equal")
